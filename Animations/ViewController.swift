42239ad3de81dcd7b2f5fdc534b392fb445ac6b0
Mehmet Ragıp Altuncu <mehmetaltuncu@gmail.com>
2016-01-17 18:05:59 -0800

    Basic animations

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -10,16 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var counter = 1
+    var timer = NSTimer()
+    var isAnimating = true
+    
+    @IBOutlet weak var alienImage: UIImageView!
+    
+    
+    
+    @IBAction func buttonPressed(sender: UIButton) {
+        
+        if isAnimating {
+            timer.invalidate()
+            isAnimating = false
+            
+        }else {
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
+            isAnimating = true
+        }
+        
+    
+    }
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
+
+    
+    }
+    
+    func doAnimation () {
+        
+        if counter == 5 {
+            counter = 1
+        }else {
+            
+            counter++
+        }
+        
+        alienImage.image = UIImage(named: "frame\(counter).png")
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidLayoutSubviews() {
+        
+        alienImage.center = CGPointMake(alienImage.center.x - 400, alienImage.center.y)
+    
     }
 
-
+    override func viewDidAppear(animated: Bool) {
+        UIView.animateWithDuration(1) { () -> Void in
+            self.alienImage.center = CGPointMake(self.alienImage.center.x + 400, self.alienImage.center.y)
+        }
+        
+        
+    
+    }
+    
 }
 
